e2e2ba0bdb449b58479ce387ac59e6463813de83
sara <143519540+slopezny00@users.noreply.github.com>
2023-12-04 09:37:46 -0500

    Add files via upload

---
+++ PizzaShop.java
@@ -0,0 +1,31 @@
+// Chapter 3 extra project
+
+import java.util.Scanner;
+
+public class PizzaShop
+{
+    public static void main(String[] args)
+    {
+        // Variables and constants
+        String customer;
+        int toppings;
+        Scanner input = new Scanner(System.in);
+        Pizza pizza1 = new Pizza();
+
+        // Input phase
+        Pizza.intro();
+        System.out.print("What name will the order be under? >> ");
+        customer = input.nextLine();
+        System.out.print("Please enter the number of toppings >> ");
+        toppings = input.nextInt();
+
+        // Processing phase
+        pizza1.enterName(customer);
+        pizza1.addToppings(toppings);
+        pizza1.calcPrice();
+
+        // Output phase
+        pizza1.summary();
+        System.out.println("Order complete! Enjoy your pizza!");
+    }
+}
